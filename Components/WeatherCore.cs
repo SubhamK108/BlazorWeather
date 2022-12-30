4840cfa43a59e728ddb42c2f404a93fd221cf763
Subham Karmakar <44800073+SubhamK108@users.noreply.github.com>
2022-12-31 01:59:24 +0530

    Updated the API

--- Components/WeatherCore.cs
+++ Components/WeatherCore.cs
@@ -2,7 +2,7 @@ namespace Components
 {
     class WeatherCore
     {
-        public static string ApiKey { get; } = "3Bvx6DuChwG7JQ0PE22SUPWxKJkGo2Hc";
+        public static string ApiKey { get; } = "ZzVRlrcIodDl1pGNtdkmyWRzstHTABC2";
         public static string ErrorMessage { get; set; } = "Fetching your location";
     }
 }
